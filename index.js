61397add6dbacb54d12d658619ceb43f34c386eb
Andrei Constantinescu <andrei.c@icloud.com>
2014-10-16 23:23:33 +0300

    Add maine index.js file.

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var compression = require('compression');
+var seo = require('mean-seo');
+
+var app = express();
+
+// Enable gzip compression.
+app.use(compression());
+
+// Enable PhantomJS SEO.
+if (process.env.REDISCLOUD_URL) {
+  // If we've got Redis available, use that.
+  app.use(seo({ cacheClient: 'rediscloud' }));
+} else {
+  // Otherwise, use regular disk-based cache.
+  app.use(seo());
+}
+
+app.use(express.static(__dirname + '/dist'));
+
+//Sitemap request.
+app.get('/sitemap.xml', function(request, response) {
+  response.sendFile(__dirname + '/dist/sitemap.xml');
+});
+
+//Asny request.
+app.get('*', function(request, response) {
+  var path = request.url.replace(/\?.*$/, '').replace(/\/$/, '').replace(/^\//, '');
+  var found = false;
+  for (var i = 0, n = validRoutes.length; i < n; i++) {
+    if (validRoutes[i] === path) {
+      found = true;
+      break;
+    }
+  }
+  response.sendFile(__dirname + '/dist/index.html');
+});
+
+//Server port setup.
+var port = process.env.PORT || 8080;
+app.listen(port, function () {
+  console.log('Listening on port: ' + port + '.');
+});
