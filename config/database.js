0c5397a5c5b5ba4f6669f8e8da207764dc7b84d6
Andrei Constantinescu <andrei.c@icloud.com>
2014-12-11 13:23:35 +0200

    Ooops.

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 
-var username = process.ENV.COMPOSE_USER ? process.ENV.COMPOSE_USER : '';
-var pass = process.ENV.COMPOSE_PASS ? process.ENV.COMPOSE_PASS : '';
+var username = process.env.COMPOSE_USER ? process.env.COMPOSE_USER : '';
+var pass = process.env.COMPOSE_PASS ? process.env.COMPOSE_PASS : '';
 var url = username && pass ? 'mongodb://' + username + ':' + pass + '@linus.mongohq.com:10082/omnifee-database' : 'localhost:27017';
 
 module.exports = {
